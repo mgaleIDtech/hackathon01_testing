13e2dd6968d197b24d7852f9f030965f37571d6f
mgaleIDtech <mgale@idtech.com>
2021-06-28 19:16:28 -0400

    Playing around with visual graphs and c#

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+
+        Debug.Log(Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W))
+            pos.y = pos.y + 1f * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            pos.y = pos.y + -1f * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.D))
+            pos.x = pos.x + 1f * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.A))
+            pos.x = pos.x - 1f * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
